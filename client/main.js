a1cb013350f353c4cf30964faad13291148c3c31
Rahul Prajapati <raarav717@gmail.com>
2020-12-30 13:30:10 +0530

    start electron

--- client/main.js
+++ client/main.js
@@ -24,7 +24,7 @@ function createWindow() {
     })
 }
 
-// app.whenReady().then(createWindow);
+app.whenReady().then(createWindow);
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
